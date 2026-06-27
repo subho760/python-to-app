35e165f8d167e54c72a36dc02dc8842e94acbce8
subho760 <nightshadow5g@gmail.com>
2026-06-27 06:05:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,45 @@
 import os
 from kivy.app import App
-from kivy.clock import Clock
-from jnius import autoclass, cast
+from kivy.uix.boxlayout import BoxLayout
+from kivy.utils import platform
 
-# Native Android Imports
-PythonActivity = autoclass('org.kivy.android.PythonActivity')
-WebView = autoclass('android.webkit.WebView')
-WebViewClient = autoclass('android.webkit.WebViewClient')
-LayoutParams = autoclass('android.view.ViewGroup$LayoutParams')
+if platform == 'android':
+    from jnius import autoclass
+    PythonActivity = autoclass('org.kivy.android.PythonActivity')
+    WebView = autoclass('android.webkit.WebView')
+    WebViewClient = autoclass('android.webkit.WebViewClient')
+    LayoutParams = autoclass('android.view.ViewGroup$LayoutParams')
 
-class WebWrapperApp(App):
-    def build(self):
-        # We return None as the UI is natively injected via WebView
-        return None
+class WebViewContainer(BoxLayout):
+    def __init__(self, **kwargs):
+        super(WebViewContainer, self).__init__(**kwargs)
+        if platform == 'android':
+            self.setup_webview()
 
-    def on_start(self):
-        # Schedule the native view injection on the main Android thread
-        Clock.schedule_once(self.create_webview, 0)
-
-    def create_webview(self, *args):
+    def setup_webview(self):
         activity = PythonActivity.mActivity
+        self.webview = WebView(activity)
+        settings = self.webview.getSettings()
         
-        # Initialize WebView
-        webview = WebView(activity)
-        settings = webview.getSettings()
         settings.setJavaScriptEnabled(True)
         settings.setDomStorageEnabled(True)
         settings.setAllowFileAccess(True)
         settings.setAllowContentAccess(True)
         
-        # Prevent opening external browser
-        webview.setWebViewClient(WebViewClient())
+        self.webview.setWebViewClient(WebViewClient())
+        activity.runOnUiThread(self.load_local_web)
 
-        # Resolve local file path
-        # In Android/Kivy, assets are unpacked into the app folder
-        current_dir = os.path.dirname(__file__)
-        index_file = os.path.join(current_dir, 'web', 'index.html')
+    def load_local_web(self):
+        activity = PythonActivity.mActivity
+        params = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
         
-        # Load local HTML
-        webview.loadUrl(f"file://{index_file}")
+        html_path = os.path.join(os.path.dirname(__file__), 'web', 'index.html')
+        self.webview.loadUrl(f"file://{html_path}")
+        activity.addContentView(self.webview, params)
 
-        # Map to Activity View
-        activity.setContentView(webview, LayoutParams(
-            LayoutParams.MATCH_PARENT, 
-            LayoutParams.MATCH_PARENT
-        ))
+class MainWrapperApp(App):
+    def build(self):
+        return WebViewContainer()
 
 if __name__ == '__main__':
-    WebWrapperApp().run()
+    MainWrapperApp().run()
