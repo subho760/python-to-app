cc9a2e03d7b39c368a5e77d3021866a7beefeaa4
subho760 <nightshadow5g@gmail.com>
2026-06-27 14:48:56 +0530

    Delete www directory

--- www/script.js
+++
@@ -1,8 +0,0 @@
-const button = document.getElementById('colorBtn');
-const colors = ['#121212', '#1a237e', '#004d40', '#3e2723', '#4a148c'];
-let currentIndex = 0;
-
-button.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % colors.length;
-    document.body.style.backgroundColor = colors[currentIndex];
-});
